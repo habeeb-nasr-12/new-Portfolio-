0842f060fac97c46716159846cd57c0f327261c8
Habeeb <habebenasr4@gmail.com>
2024-07-27 09:33:27 +0300

    change env var names

--- src/client.js
+++ src/client.js
@@ -2,11 +2,11 @@ import sanityClient from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 
 export const client = sanityClient({
-  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+  projectId: process.env.PROJECTID,
   dataset: "production",
   apiVersion: "2024-07-22",
   useCdn: true,
-  token: process.env.REACT_APP_SANITY_PROJECT_TOKEN,
+  token: process.env.PROJECTTOKEN,
 });
 
 const builder = imageUrlBuilder(client);
