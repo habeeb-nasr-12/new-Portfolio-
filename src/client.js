c98107ae5cad03ec70c126e30897ad495f7afe4e
Habeeb <habebenasr4@gmail.com>
2024-07-27 22:30:24 +0300

    updating env vars

--- src/client.js
+++ src/client.js
@@ -1,12 +1,19 @@
 import sanityClient from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 
+
+
+console.log('Project ID:', process.env.REACT_APP_SANITY_PROJECT_ID);
+
+if (!process.env.REACT_APP_SANITY_PROJECT_ID) {
+  throw new Error('Sanity Project ID is missing. Check your .env file.');
+}
 export const client = sanityClient({
-  projectId: process.env.PROJECTID,
+  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
   dataset: "production",
   apiVersion: "2024-07-22",
   useCdn: true,
-  token: process.env.PROJECTTOKEN,
+  token: process.env.REACT_APP_SANITY_PROJECT_TOKEN ,
 });
 
 const builder = imageUrlBuilder(client);
